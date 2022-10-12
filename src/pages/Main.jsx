7b5ea3ea01693e1158788d88ffbe9eb21ff46773
mustafa-3 <sagunmustafa14@gmail.com>
2022-10-12 11:09:51 +0300

    v6

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,74 +1,11 @@
-import { Button, TextField } from "@mui/material";
-import { Box } from "@mui/system";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Navbar from "../components/Navbar";
-import { db } from "../auth/Firebase";
-import { ref, push, set, onValue, remove } from "firebase/database";
 
 const Main = () => {
-  const [name, setName] = useState();
-  const [contact, setContact] = useState();
-  const [blogList, setBlogList] = useState([]);
-
-  const writeToDatabase = () => {
-    const blogRef = ref(db, "blogs");
-    const newBlogRef = push(blogRef);
-    set(newBlogRef, {
-      name: name,
-      contact: contact,
-    });
-  };
-
-  const deleteFromDatabase = (item) => {
-    remove(ref(db, `blogs/${item.id}`));
-  };
-
-  useEffect(() => {
-    const blogRef = ref(db, "blogs");
-    onValue(blogRef, (snapshot) => {
-      const data = snapshot.val();
-      const blogArray = [];
-      for (let id in data) {
-        blogArray.push({
-          id,
-          ...data[id],
-        });
-      }
-      setBlogList(blogArray);
-    });
-  }, []);
-
-  console.log(blogList);
-
   return (
     <>
       <Navbar />
-      <form onSubmit={writeToDatabase}>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            gap: 4,
-            marginTop: 4,
-          }}
-        >
-          <TextField onChange={(e) => setContact(e.target.value)} />
-          <TextField onChange={(e) => setName(e.target.value)} />
-
-          <Button type="submit">Submit</Button>
-        </Box>
-      </form>
-
-      {blogList.map((item, index) => {
-        return (
-          <div key={index}>
-            <div>{item.name}</div>
-            <div>{item.contact}</div>
-            <Button onClick={() => deleteFromDatabase(item)}>Remove</Button>
-          </div>
-        );
-      })}
+      <div>Main</div>
     </>
   );
 };
--- src/pages/NewBlog.jsx
+++ src/pages/NewBlog.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
-
+import React from "react";
+import Navbar from "../components/Navbar";
+import blogImage from "../assets/blogImage.png";
+import { Box, Container } from "@mui/system";
+import { Button, CardMedia, TextField, Typography } from "@mui/material";
 const NewBlog = () => {
   return (
-    <div>NewBlog</div>
-  )
-}
+    <>
+      <Navbar />
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "calc(100vh - 100px)",
+        }}
+      >
+        <CardMedia
+          component="img"
+          sx={{ width: "14rem" }}
+          image={blogImage}
+          alt="green iguana"
+        />
+        <Typography sx={{ margin: ".5rem", fontSize: "1.3rem" }}>
+          New Blog
+        </Typography>
+        <Box component="form" noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="title"
+            label="title"
+            name="title"
+            autoFocus
+            placeholder="Add a title to your Blog"
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="Image"
+            label="Image"
+            type="text"
+            id="Image"
+            placeholder="Image URL"
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="content"
+            label="content"
+            name="content"
+            autoFocus
+            placeholder="Please enter your content"
+          />
 
-export default NewBlog
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            ADD NEW BLOG
+          </Button>
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default NewBlog;
